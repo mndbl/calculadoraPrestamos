2cb11d768ece96358920a73d56478efd5e0ad480
mndbl <augusto0309@gmail.com>
2021-02-12 03:41:29 -0400

    Método Francés

--- resources/views/livewire/calcular-prestamo.blade.php
+++ resources/views/livewire/calcular-prestamo.blade.php
@@ -4,6 +4,7 @@
     </div>
 
     <div class="sm:mx-auto lg:mr-2 mt-3 sm:w-full lg:w-2/3">
+        @if($calculado)
         <table class="table sm:w-auto">
             <thead>
                 <tr>
@@ -15,7 +16,7 @@
                 </tr>
             </thead>
             <tbody>
-                @if($calculado)
+
                 @for($i = 0; $i < $plazo; $i++) <tr class="">
                     <td class="p-2 text-center">{{$i+1}}</td>
                     <td class="p-2 text-right mr-2">{{ number_format($cuota, 2, ',', '.')}}</td>
@@ -30,9 +31,9 @@
                     <tr class="">
                         <td class="p-2 text-left mr-2" colspan=5>Monto Total Intereses Generados: {{number_format($cuota * $plazo - $capital , 2, ',', '.') }}</td>
                     </tr>
-                    @endif
             </tbody>
         </table>
+        @endif
     </div>
 
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,19 @@
 @section('content')
 <!-- component -->
 
-<div class="pt-2 bg-indigo-600 rounded-t-lg bg-opacity-70 lg:mx-2 mt-2 sm:w-full">
+<div class="pt-2 h-1/5 bg-indigo-600 rounded-lg bg-opacity-70 lg:mx-2 sm:w-full">
     <h1 class="block text-white font-bold text-xl text-center -mb-2">Calculadora de Préstamos</h1><br>
+
+    <livewire:calcular-prestamo>
 </div>
-<livewire:calcular-prestamo>
-    <div class="lg:flex mb-2 mt-2 rounded-b-lg sm:mx-auto lg:mx-2 pl-4 bg-indigo-600 lg:text-left text-sm text-white py-2 sm:w-full">
-        <div>
-            <p class="hover:font-bold">Laravel v8.26.1 (PHP v7.4.13)</p>
+<div class="fixed bottom-0 pt-2 bg-indigo-600 rounded-b-lg bg-opacity-50 lg:mx-2 w-full">
+    <div class="w-full flex gap-2 items-center">
+        <div class="hover:font-bold">
+            <p class="ml-2 sm:text-sm">Laravel v8.26.1 (PHP v7.4.13) / </p>
         </div>
-        <div><a class="hover:font-bold" href="https://github.com/mndbl/calculadoraPrestamos" target="_blank">mndbl en Github</a></div>
+        <div class="hover:font-bold">
+            <p class="sm:text-sm"><a href="https://github.com/mndbl/calculadoraPrestamos" target="_blank">mndbl en Github</a></p>
+        </div>
     </div>
-    @endsection
+</div>
+@endsection
